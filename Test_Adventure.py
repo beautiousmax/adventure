6bb06465337ad0e2e65ee8b0420db1fa68983dff
beautiousmax <ericcl01@gmail.com>
2013-12-21 15:50:17 -0500

    changed how the map is generated, now the navigation mechanism is broken.

--- Test_Adventure.py
+++ Test_Adventure.py
@@ -6,11 +6,6 @@ import adventure_map
 
 class TestForGreatness(unittest.TestCase):
 
-    def test_been_here_yet(self):
-        self.assertTrue(adventure_map.current.discovered_yet is True)
-        adventure_map.current = adventure_map.change_direction("east")
-        self.assertTrue(adventure_map.current.discovered_yet is True)
-
     def test_change_direction(self):
         x = adventure_map.current.location
         self.assertTrue(x in adventure_map.grid)
